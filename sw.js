3dedcea49dc2450757833dbde2ab1199d6f87cd3
schafferjrdev <mr.giancarloschaffer@gmail.com>
2019-10-20 14:38:45 -0300

    Service Worker working!

--- sw.js
+++ sw.js
@@ -1,1 +1,23 @@
-workbox.precaching.precacheAndRoute(__precacheManifest);
+var cacheName = 'phaser-v3';
+var filesToCache = [
+  '/',
+  '/index.html',
+  '/public/favicon.ico',
+  '/public/cursor.cur',
+  '/public',
+];
+
+self.addEventListener('install', function(event) {
+  console.log('sw install');
+  event.waitUntil(
+    caches
+      .open(cacheName)
+      .then(function(cache) {
+        console.log('sw caching files');
+        return cache.addAll(filesToCache);
+      })
+      .catch(function(err) {
+        console.log(err);
+      })
+  );
+});
