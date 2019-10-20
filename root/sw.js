0b351d3f076141249b8c2223915301a35251041c
schafferjrdev <mr.giancarloschaffer@gmail.com>
2019-10-20 14:55:19 -0300

    Add fetch and activate on SW

--- root/sw.js
+++ root/sw.js
@@ -8,16 +8,47 @@ var filesToCache = [
 ];
 
 self.addEventListener('install', function(event) {
-  console.log('sw install');
+  console.info('sw install');
   event.waitUntil(
     caches
       .open(cacheName)
       .then(function(cache) {
-        console.log('sw caching files');
+        console.info('sw caching files');
         return cache.addAll(filesToCache);
       })
       .catch(function(err) {
-        console.log(err);
+        console.error(err);
       })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  console.info('sw fetch');
+  console.info(event.request.url);
+  event.respondWith(
+    caches
+      .match(event.request)
+      .then(function(response) {
+        return response || fetch(event.request);
+      })
+      .catch(function(error) {
+        console.error(error);
+      })
+  );
+});
+
+self.addEventListener('activate', function(event) {
+  console.info('sw activate');
+  event.waitUntil(
+    caches.keys().then(function(keyList) {
+      return Promise.all(
+        keyList.map(function(key) {
+          if (key !== cacheName) {
+            console.info('sw removing old cache', key);
+            return caches.delete(key);
+          }
+        })
+      );
+    })
   );
 });
